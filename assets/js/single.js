2c081c00d3146e087c2efd0cb9b7ba80a9b0d550
Liss Diaz <lissdiaz15@gmail.com>
2021-11-26 16:41:23 -0700

    single.html now connect with index.html

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,18 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if(repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+} else {
+  document.location.replace("./index.html");
+}
+};
 
 var getRepoIssues = function(repo) { 
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction-asc";
@@ -19,7 +31,7 @@ var getRepoIssues = function(repo) {
     });
   }
   else {
-    alert("There was a problem with your request!");
+    document.location.replace("./index.html");
   }
   });
 };
@@ -73,4 +85,4 @@ var displayIssues = function(issues) {
   limitWarningEl.appendChild(linkEl);
   };
 
-getRepoIssues("facebook/react");
+getRepoName();
